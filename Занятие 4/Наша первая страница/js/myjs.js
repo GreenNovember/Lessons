8ba156033da38c8b6b4b9d76c88cd15ad8d4f930
Татьяна Юрченко <soulrain421@mail.ru>
2016-02-06 19:19:03 +0300

    Добавлено 5 занятие

---
+++ Занятие 4/Наша первая страница/js/myjs.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var a = $("a");
+	a.mouseover(function(){$(this).css({"color":"#44a7d4","text-decoration":"underline"})
+});
+	a.mouseout(function(){$(this).css({"color":"#0c4a43","text-decoration":"none"})
+});
+	
+
+
+
+
+
+});//конец ready
